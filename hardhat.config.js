8b2509efec49209bb015eaf07ce47accd4d1c6c3
Misha <pochitalin.m@gmail.com>
2023-11-10 23:41:02 +0300

    Step-1: testing network config changed

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
-      url: process.env.GOERLI_URL,
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
   }
